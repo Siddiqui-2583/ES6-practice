72a9ab02083507018b406637b7c4a9d4cbe2f958
Siddiqui-2583 <nurnaby83@gmail.com>
2020-03-03 11:31:27 +0600

    letconst done.

---
+++ letconst.js
@@ -0,0 +1,26 @@
+const hubby = "Elias kanchon";
+//hubby = "Omar sunny";
+console.log(hubby);
+
+
+const numbers = [12,45];
+numbers[1]=88;
+numbers.push(12);
+//numbers = ["Sunny","Salman","Omar"];
+console.log(numbers);
+
+const nayok = {name:"Sakib",phone:4588};
+nayok.name="Rakib";
+console.log(nayok.name);
+
+let patientName = "Rahim chacha";
+patientName = "Fatema khala";
+console.log(patientName);
+
+let sum =0;
+for(var i = 0;i < 10; i++){
+    sum = sum + i;
+}
+console.log(i);
+
+//const hocche constant, let mane changeable but lick hobe na(scope variable)
